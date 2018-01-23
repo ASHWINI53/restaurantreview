cc7ffed4e3bd1c6e58926d87bf70df0bd01aa15c
ASHWINI53 <ashwiniiral16@gmail.com>
2018-01-23 21:14:32 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  background-image: url("./assets/restaurant-review.jpg");
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+body, html {
+  height: 100%;
+  margin: 0;
+}
+</style>
